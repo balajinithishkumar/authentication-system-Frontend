3bdec356fdb15e8fe94bd872f2d18776acfe70ab
balajinithishkumar <bnithish213@gmail.com>
2023-12-14 18:36:01 +0530

    authentication frontend project

---
+++ src/userValidation.js
@@ -0,0 +1,11 @@
+export function userValidation(user) {
+  if (user.data?.Error) return user.data.Error;
+  localStorage.setItem("user", JSON.stringify(user.data));
+}
+
+export function logoutvalidation(user) {
+  if (user.data?.Error) return alert(user.data.Error);
+  alert("Loged Out!");
+  localStorage.setItem("user", null);
+  return window.location.reload();
+}
